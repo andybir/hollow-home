8972be11530d82c6db1741085af7127e408d9fbb
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2022-05-25 13:04:59 -0400

    initial commit

---
+++ app/root.jsx
@@ -0,0 +1,76 @@
+import {
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
+import globalStylesUrl from '~/styles/global.css'
+
+export const links = () => [{ rel: 'stylesheet', href: globalStylesUrl }]
+
+export const meta = () => {
+  const description = 'The homepage for the hollow early learning center'
+  const keywords = 'the hollow, daycare, child care'
+  const title = 'the hollow'
+  return { description, keywords, title }
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charset='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <script
+          src='https://kit.fontawesome.com/25f620e40e.js'
+          crossorigin='anonymous'
+        ></script>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+      </body>
+    </html>
+  )
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='logo'>
+          the hollow
+        </Link>
+      </nav>
+      <div className='container'>{children}</div>
+    </>
+  )
+}
+
+export function ErrorBoundary({ error }) {
+  console.log(error)
+  return (
+    <Document>
+      <Layout>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  )
+}
---
+++ app/routes/index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to the hollow!</h1>
+      <h2><a href="https://sullivankids.org/">Sullivan Kids</a></h2>
+      <h2><a href="https://thehollowbk.org/">the hollow</a></h2>
+    </div>
+  )
+}
