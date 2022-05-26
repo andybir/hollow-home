1c5a291f903862c7bb2417f338bd291d4a2d7303
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2022-05-26 12:05:14 -0400

    formatting home page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,17 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to the hollow!</h1>
-      <h2><a href="https://sullivankids.org/">Sullivan Kids</a></h2>
-      <h2><a href="https://thehollowbk.org/">the hollow</a></h2>
+    <div className='home-container'>
+      <div className='title-container'>
+        <h1>Welcome to the hollow!</h1>
+      </div>
+      <div className='links-container'>
+        <div>
+          <a href='https://sullivankids.org/'>Sullivan Kids</a>
+        </div>
+        <div>
+          <a href='https://thehollowbk.org/'>the hollow</a>
+        </div>
+      </div>
     </div>
   )
 }
