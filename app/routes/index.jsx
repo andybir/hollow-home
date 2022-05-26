ea922f86bec4b760096f0e9730bd891698db4ad1
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2022-05-26 14:38:07 -0400

    added bird logo png

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,14 @@
+import logo from '../../public/images/hollow-logo-lg.png'
+
 export default function Index() {
   return (
     <div className='home-container'>
+      <div className='logo-container'>
+        <img className='logo-png' src={logo} alt='' srcset='' />
+      </div>
       <div className='title-container'>
         <h1>Welcome to</h1>
-        <h1 className="the-hollow">the hollow!</h1>
+        <h1 className='the-hollow'>the hollow!</h1>
       </div>
       <div className='links-container'>
         <a href='https://sullivankids.org/'>
